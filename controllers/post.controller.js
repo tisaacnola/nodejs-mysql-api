0d3cbd6cb50632e1fa28a7028c49ccdb3039cb89
tisaacnola <oisaac47@gmail.com>
2024-10-12 11:58:41 -0500

    completed crud functions

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -23,6 +23,101 @@ function save(req, res) {
     });
 }
 
+function show(req, res) {
+  const id = req.params.id;
+
+  models.Post.findByPk(id)
+    .then((result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({
+          message: "Post not found!",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: error,
+      });
+    });
+}
+
+function index(req, res) {
+  models.Post.findAll()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: error,
+      });
+    });
+}
+
+function update(req, res) {
+  const id = req.params.id;
+  const updatedPost = {
+    title: req.body.title,
+    content: req.body.content,
+    imageUrl: req.body.image_url,
+    categoryId: req.body.category_id,
+  };
+
+  const userId = 1;
+
+  models.Post.update(updatedPost, { where: { id: id, userId: userId } })
+    .then((result) => {
+      if (result) {
+        res.status(201).json({
+          message: "Post updated successfully",
+          post: updatedPost,
+        });
+      } else {
+        res.status(404).json({
+          message: "Post not found!",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: error,
+      });
+    });
+}
+
+function destroy(req, res) {
+  const id = req.params.id;
+  const userId = 1;
+
+  models.Post.destroy({ where: { id: id, userId: userId } })
+    .then((result) => {
+      if (result) {
+        console.log(result);
+        res.status(201).json({
+          message: "Post successfully destroyed",
+        });
+      } else {
+        res.status(404).json({
+          message: "Post not found!",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: error,
+      });
+    });
+}
+
 module.exports = {
   save: save,
+  show: show,
+  index: index,
+  update: update,
+  destroy: destroy,
 };
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,9 @@ const postsController = require("../controllers/post.controller");
 const router = express.Router();
 
 router.post("/", postsController.save);
+router.get("/", postsController.index);
+router.get("/:id", postsController.show);
+router.patch("/:id", postsController.update);
+router.delete("/:id", postsController.destroy);
 
 module.exports = router;
