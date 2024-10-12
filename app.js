8485d94de3c30d8292a571c3f3ab15ca50152644
tisaacnola <oisaac47@gmail.com>
2024-10-12 13:20:39 -0500

    implemented sign-up and checked if email was already in database

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
 const postsRoute = require("./routes/posts");
 const commentsRoute = require("./routes/comments");
+const userRoute = require("./routes/user");
 
 app.use(bodyParser.json());
 
 app.use("/posts", postsRoute);
 app.use("/comments", commentsRoute);
+app.use("/user", userRoute);
 
 module.exports = app;
