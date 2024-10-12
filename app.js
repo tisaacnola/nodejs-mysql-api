049c117099ae85e0bcb7cd7517f7ad5b9bb2e606
tisaacnola <oisaac47@gmail.com>
2024-10-12 10:24:23 -0500

    created save route for post

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
-
 const postsRoute = require("./routes/posts");
+
+app.use(bodyParser.json());
 
 app.use("/posts", postsRoute);
 
--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,8 +1,28 @@
-function index(req, res) {
-  const posts = "Posts list";
-  res.send(posts);
+const models = require("../models");
+
+function save(req, res) {
+  const post = {
+    title: req.body.title,
+    content: req.body.content,
+    imageUrl: req.body.image_url,
+    categoryId: req.body.category_id,
+    userId: 1,
+  };
+  models.Post.create(post)
+    .then((result) => {
+      res.status(201).json({
+        message: "Post created successfully",
+        post: result,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: error,
+      });
+    });
 }
 
 module.exports = {
-  index: index,
+  save: save,
 };
