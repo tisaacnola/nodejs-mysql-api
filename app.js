82161c7dd5ce1698555c0e6ebac57fe857e7d42d
tisaacnola <oisaac47@gmail.com>
2024-10-12 08:18:02 -0500

    added a post route and controller

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, world");
-});
+const postsRoute = require("./routes/posts");
+
+app.use("/posts", postsRoute);
 
 module.exports = app;
