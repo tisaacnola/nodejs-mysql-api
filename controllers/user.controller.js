2baee3dbb6c27f254981fbc8c4354c15ea1197ac
tisaacnola <oisaac47@gmail.com>
2024-10-12 13:54:14 -0500

    implemented signUp/login with validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const bcryptjs = require("bcryptjs");
 const models = require("../models");
 const jwt = require("jsonwebtoken");
+const Validator = require("fastest-validator");
 
 function signUp(req, res) {
   models.User.findOne({ where: { email: req.body.email } })
@@ -17,7 +18,21 @@ function signUp(req, res) {
               email: req.body.email,
               password: hash,
             };
+            const schema = {
+              name: { type: "string", optional: false, max: "100" },
+              email: { type: "string", optional: false, max: "500" },
+              password: { type: "string", optional: false },
+            };
 
+            const v = new Validator();
+            const validationResponse = v.validate(user, schema);
+
+            if (validationResponse !== true) {
+              return res.status(400).json({
+                message: "validation failed",
+                error: validationResponse,
+              });
+            }
             models.User.create(user)
               .then((result) => {
                 res.status(201).json({
@@ -33,9 +48,56 @@ function signUp(req, res) {
         });
       }
     })
-    .catch((error) => {});
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+      });
+    });
+}
+
+function login(req, res) {
+  models.User.findOne({ where: { email: req.body.email } })
+    .then((user) => {
+      if (user === null) {
+        res.status(401).json({
+          message: "Invalid credentials",
+        });
+      } else {
+        bcryptjs.compare(
+          req.body.password,
+          user.password,
+          function (err, result) {
+            if (result) {
+              const token = jwt.sign(
+                {
+                  email: user.email,
+                  userId: user.id,
+                },
+                "secret",
+                function (err, token) {
+                  res.status(200).json({
+                    message: "Authentication successful",
+                    token: token,
+                  });
+                }
+              );
+            } else {
+              res.status(401).json({
+                message: "Invalid credentials",
+              });
+            }
+          }
+        );
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+      });
+    });
 }
 
 module.exports = {
   signUp: signUp,
+  login: login,
 };
